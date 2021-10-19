124f736d4adbd79256388dd637cec4852ce61f4f
Jaynagracetan <jaynagracetan@gmail.com>
2021-10-19 17:08:36 +0800

    Black, Red. Blue
    Changing to have the words, Black, Red, Blue reflect the color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+
+export default function App() {
+  const [black, setBlack] = useState(true);
+  const [red, setRed] = useState(true);
+  const [blue, setBlue] = useState(true);
+
+  const showBlack = () => {
+    setBlack(true);
+    setRed(false);
+    setBlue(false);
+  };
+
+  const showRed = () => {
+    setBlack(false);
+    setRed(true);
+    setBlue(false);
+  };
+
+  const showBlue = () => {
+    setBlack(false);
+    setRed(false);
+    setBlue(true);
+  };
+
+  const renderBackground = () => {
+    if (black) {
+      return <div className = "blackColor"/>;
+    } else if (blue){
+      return <div className = "blueColor"/>;
+    } else {
+      return <div className = "redColor"/>;
+    } 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, my name is jay
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+        <div className = "leftRect">
+          <div className="square">
+            <div onClick={showBlack}>
+              <h3>Black</h3>
+            </div>
+          </div>
+          <div className="square">
+            <div onClick={showRed}>
+              <h3>Red</h3>
+            </div>
+          </div>
+          <div className="square">
+            <div onClick={showBlue}>
+              <h3>Blue</h3>
+            </div>
+          </div>
+        </div> 
+      <div className = "blackColor">
+            { renderBackground() }
+      </div>
     </div>
-  );
-}
+  )
 
-export default App;
+}
