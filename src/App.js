f49f99990652ba386dbe906dd363cb4e9e8c9ad2
Joel Jonas <Joeljonast@gmail.com>
2021-10-20 15:38:11 +0800

    commit some changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ export default function App() {
   const [red, setRed] = useState(true);
   const [blue, setBlue] = useState(true);
 
+  const [color, setColor] = useState(0);
+
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    gender: 0,
+    subscription: true,
+  });
+
+  const [nameInput, setNameInput] = useState('');
+
+  const jsObject = {
+    textInput : '',
+    color: 0
+  };
+
+
   const showBlack = () => {
     setBlack(true);
     setRed(false);
@@ -36,9 +54,26 @@ export default function App() {
     } 
   }
 
+  const selectColor = (newColor) => {
+    setColor(newColor);
+  }
+
+  const renderBackgroundVersionTwo = () => {
+    switch (color) {
+      case 0:
+        return <div className="blackColor" />;
+      case 1:
+        return <div className="redColor" />;
+      case 2:
+        return <div className="blueColor" />;
+      default:
+        return <div className="blackColor" />;
+    }
+  };
+
   return (
     <div className = "App">
-      { renderBackground() }
+      { renderBackgroundVersionTwo() }
         <div className = "leftRect">
           <div className="square">
             <div onClick={showBlack}>
@@ -55,6 +90,46 @@ export default function App() {
               <h3>Blue</h3>
             </div>
           </div>
+        <div className="square">
+          <div onClick={() => selectColor(0)}>
+            <h3>New Black</h3>
+          </div>
+        </div>
+        <div className="square">
+          <div onClick={() => selectColor(1)}>
+            <h3>New Red</h3>
+          </div>
+        </div>
+        <div className="square">
+          <div onClick={() => selectColor(2)}>
+            <h3>New Blue</h3>
+          </div>
+        </div>
+        name: 
+        <input
+          value={state.name}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setState(
+              {
+                ...state,
+                name: e.target.value
+              }
+            );
+          }}
+        />
+        email: <input 
+          value={state.email}
+          onChange={(e) => {
+            setState({
+              ...state,
+              email: e.target.value
+            });
+          }}
+        />
+        phone: <input 
+          value={state.phone}
+        />
         </div> 
       {/* <div className = "blackColor">
             
